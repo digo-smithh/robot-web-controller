dc1baa144e574d906db2a6a2a097b352948e15db
digo-smithh <smithrodrigues08@gmail.com>
2021-05-19 17:41:07 -0300

    Edit

---
+++ Website/app.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, './app/public', 'home.html'));
+});
+
+app.get("/picture", (req, res) => {
+  res.sendFile(path.join(__dirname, './app/public', 'picture.html'));
+  io.emit('picRequest', "picRequest");
+});
+
+app.use('/static', express.static(__dirname + '/app/public'));
+
+// set port, listen for requests
+app.listen(4000, () => {
+  console.log("Server is running on port 4000.");
+});
